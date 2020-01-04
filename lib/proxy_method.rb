6d509f68ebf4eb63bd88b9131427409072654284
Jaime Bellmyer <online@bellmyer.com>
2020-01-04 10:13:03 -0600

    privateize all methods not part of the public API

--- lib/proxy_method.rb
+++ lib/proxy_method.rb
@@ -3,19 +3,6 @@ module ProxyMethod
     DEFAULT_PROXY_MESSAGE = 'Disabled by proxy_method'
     DEFAULT_PREFIX = 'unproxied_'
 
-    def proxied_instance_methods
-      @_proxied_instance_methods ||= {}
-    end
-
-    def proxied_class_methods
-      @_proxied_class_methods ||= {}
-    end
-
-    def proxy_class_methods_enabled?
-      return @_proxy_class_methods_enabled if defined?(@_proxy_class_methods_enabled)
-      @_proxy_class_methods_enabled = true
-    end
-
     def proxy_class_method(*original_method_names, &proxy_block)
       options = if original_method_names.last.is_a?(Hash)
         original_method_names.pop
@@ -29,7 +16,7 @@ module ProxyMethod
       prefix = options[:prefix] || DEFAULT_PREFIX
 
       original_method_names.each do |original_method_name|
-        self.proxied_class_methods.merge!(original_method_name => prefix)
+        proxied_class_methods.merge!(original_method_name => prefix)
         new_method_name = :"#{prefix}#{original_method_name}"
 
         self.singleton_class.send(:alias_method, new_method_name, original_method_name)
@@ -60,7 +47,7 @@ module ProxyMethod
       prefix = options[:prefix] || DEFAULT_PREFIX
 
       original_method_names.each do |original_method_name|
-        self.proxied_instance_methods.merge!(original_method_name => prefix)
+        proxied_instance_methods.merge!(original_method_name => prefix)
         new_method_name = :"#{prefix}#{original_method_name}"
 
         alias_method new_method_name, original_method_name
@@ -104,6 +91,19 @@ module ProxyMethod
 
     private
 
+    def proxy_class_methods_enabled?
+      return @_proxy_class_methods_enabled if defined?(@_proxy_class_methods_enabled)
+      @_proxy_class_methods_enabled = true
+    end
+
+    def proxied_instance_methods
+      @_proxied_instance_methods ||= {}
+    end
+
+    def proxied_class_methods
+      @_proxied_class_methods ||= {}
+    end
+
     def unproxy! # :nodoc:
       @_proxy_class_methods_enabled = false
       self
@@ -119,11 +119,6 @@ module ProxyMethod
     base.extend ClassMethods
   end
 
-  def proxy_instance_methods_enabled?
-    return @_proxy_instance_methods_enabled if defined?(@_proxy_instance_methods_enabled)
-    @_proxy_instance_methods_enabled = true
-  end
-
   def unproxied
     self.dup.send(:unproxy!)
   end
@@ -133,6 +128,11 @@ module ProxyMethod
   end
 
   private
+
+  def proxy_instance_methods_enabled?
+    return @_proxy_instance_methods_enabled if defined?(@_proxy_instance_methods_enabled)
+    @_proxy_instance_methods_enabled = true
+  end
 
   def unproxy!
     @_proxy_instance_methods_enabled = false
